882ce10d158ee9f0f4a40554155779f44e46fd15
Aaron Bader <abader@engr.wisc.edu>
2018-10-29 07:53:12 -0500

    initial commit

---
+++ read_boozmn.py
@@ -0,0 +1,115 @@
+from netCDF4 import Dataset
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.interpolate as interp
+
+
+
+class boozer:
+    def __init__(self, fname):
+        self.data = Dataset(fname)
+        self.bmnc = np.array(self.data.variables['bmnc_b'][:])
+        self.xm =  np.array(self.data.variables['ixm_b'][:])
+        self.xn =  np.array(self.data.variables['ixn_b'][:])
+        self.phi = np.array(self.data.variables['phi_b'][:])
+        self.nfp = np.array(self.data.variables['nfp_b'])
+        self.It = np.array(self.data.variables['bvco_b'])
+        self.Ip = np.array(self.data.variables['buco_b'])
+        self.s = self.phi/self.phi[-1]
+        self.nrbooz =  len(self.bmnc) 
+        self.nr = len(self.phi)
+        self.sr = self.s[self.nr-self.nrbooz:]
+        self.mnmodes = len(self.xm)
+        self.interp_at = -1
+        self.charge = 1.602E-19
+
+    def interp_bmn(self, s):
+        self.binterp = np.empty(self.mnmodes)
+        self.dbdpsi = np.empty(self.mnmodes)
+        for i in xrange(self.mnmodes):
+            bspl = interp.UnivariateSpline(self.sr, self.bmnc[:,i])
+            #self.binterp[i] = interp.griddata(self.sr,self.bmnc[:,i],
+            #                                  s,method='cubic')
+            self.binterp[i] = bspl(s)
+            #print bspl.derivatives(s)
+            #self.dbdpsi[i] = bspl.derivatives(s)
+        self.interp_at = s
+            
+            
+    def field_at_point(self, s,theta,zeta):
+        #make sure we've interpolated at the desired value
+        if self.interp_at != s:
+            self.interp_bmn(s)
+        b = 0
+        for i in xrange(self.mnmodes):
+            if self.xn[i] > 5 or self.xm[i] > 5:
+                continue
+            angle = self.xm[i]*theta - self.xn[i]*zeta
+            b += self.binterp[i] * np.cos(angle)
+        return b
+
+    def currents_and_derivs(self, s):
+        ipspl = interp.UnivariateSpline(self.s, self.Ip)
+        ip = np.empty(2)
+        ip[0] = ipspl(s)
+        ip[1] = ipspl.derivatives(s)
+        itspl = interp.UniveraiteSpline(self.s, self.It)
+        it = np.empty(2)
+        it[0] = itspl(s)
+        it[1] = itspl.derivatives(s)
+        return ip,it
+
+    # return b as (modb, dbdpsi, dbdtheta, dbdzeta)
+    def field_and_derivs(self, s,theta,zeta):
+        if self.interp_at != s:
+            self.interp_bmn(s)
+        b = np.zeros(4)
+        for i in xrange(self.mnmodes):
+            #if self.xn[i] > 5 or self.xm[i] > 5:
+            #    continue
+            angle = self.xm[i]*theta - self.xn[i]*zeta
+            b[0] += self.binterp[i] * np.cos(angle)
+            #b[1] += self.dbdpsi[i] * np.cos(angle)
+            b[2] += -self.xm[i] * self.binterp[i] * np.sin(angle)
+            b[3] += self.xn[i] * self.binterp[i] * np.sin(angle)
+        return b
+
+    def dpsidt(self,s,theta,zeta):
+        #simple calculation doesn't need this
+        #ip, it = self.currents_and_derivs(s)
+        b = self.field_and_derivs(s, theta, zeta)
+        # we need to know rho_parallel, or do the bounce average
+        #gamma = self.charge*(
+        return b[2]
+
+    def make_modb_contour(self, s, ntheta, nzeta):
+        theta = np.linspace(0,2*np.pi,ntheta)
+        zeta = np.linspace(0,2*np.pi,nzeta)
+        b = np.empty([ntheta,nzeta])
+        for i in xrange(nzeta):           
+            for j in xrange(ntheta):
+                b[j,i] = self.field_at_point(s, theta[j], zeta[i])
+            print zeta[i], theta[j], b[j,i]    
+                
+
+        plt.contour(zeta, theta, b, 20)
+        plt.colorbar()
+        plt.show()
+
+    def make_dpsidt_contour(self, s, ntheta, nzeta):
+        theta = np.linspace(0,2*np.pi,ntheta)
+        zeta = np.linspace(0,2*np.pi,nzeta)
+        psidot = np.empty([ntheta,nzeta])
+        for i in xrange(nzeta):           
+            for j in xrange(ntheta):
+                psidot[j,i] = self.dpsidt(s,theta[j],zeta[i])
+            print zeta[i], theta[j], psidot[j,i]
+        plt.contour(zeta, theta, psidot, 20)
+        plt.colorbar()
+        plt.show()
+            
+#bz = boozer('boozmn_qhgc.nc')
+bz = boozer('boozmn_qhs46_mn8.nc')
+#bz.make_modb_contour(0.5,51,51)
+bz.make_dpsidt_contour(0.5, 81, 81)
+
---
+++ read_vmec.py
@@ -0,0 +1,87 @@
+from netCDF4 import Dataset
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.optimize import fsolve
+
+
+class vmec_data:
+    def __init__(self, fname):
+        self.data = Dataset(fname)
+        self.rmnc = np.array(self.data.variables['rmnc'][:])
+        self.zmns = np.array(self.data.variables['zmns'][:])
+        self.lmns = np.array(self.data.variables['lmns'][:])
+        self.bmnc = np.array(self.data.variables['bmnc'][:])
+        self.xm = np.array(self.data.variables['xm'][:])
+        self.xn = np.array(self.data.variables['xn'][:])
+        self.xmnyq = np.array(self.data.variables['xm_nyq'][:])
+        self.xnnyq = np.array(self.data.variables['xn_nyq'][:])
+        self.nfp = np.array(self.data.variables['nfp'])
+        self.psi = np.array(self.data.variables['phi'])
+        self.ns = len(self.psi)
+        self.iota = np.array(self.data.variables['iotas'])
+
+
+    def plot_iota(self, show=False):
+        s = self.psi[1:]/self.psi[-1]
+        plt.plot(s, self.iota[1:])
+        if show:
+            plt.show()
+        
+    # plot 
+    def fsplot(self, phi=0, fs=-1, ntheta = 50, plot=True, show=False):
+        theta = np.linspace(0,2*np.pi,num=ntheta+1)
+       
+        r = np.zeros(ntheta+1)
+        z = np.zeros(ntheta+1)
+        for i in xrange(len(self.xm)):
+            m = self.xm[i]
+            n = self.xn[i]
+            angle = m*theta - n*phi
+            r += self.rmnc[fs,i]*np.cos(angle)
+            z += self.zmns[fs,i]*np.sin(angle)
+
+        if plot:
+            plt.plot(r,z)
+            plt.axis('equal')
+            if show:
+                plt.show()
+        return r,z
+
+    def modb_at_point(self, fs, theta, phi):
+        return sum(self.bmnc[fs,:]*np.cos(self.xmnyq*theta - self.xnnyq*phi))
+        
+            
+
+    def modb_on_fieldline(self, fs, show=False):
+        
+        phimax = 4*np.pi
+        npoints = 1001
+        iota = self.iota[fs]
+        phi = np.linspace(0,phimax,npoints)
+        thetastar = phi*iota
+        theta = np.zeros(npoints)
+        modB = np.zeros(npoints)
+        rstart = sum(self.rmnc[fs,:])
+
+        def theta_solve(x):
+            lam = self.lmns[fs,:]
+            lam1 = sum(lam*np.sin(self.xm*x - self.xn*phi[i]))
+            return x + lam1 - thetastar[i]
+        
+        for i in xrange(npoints):
+            theta[i] = fsolve(theta_solve, thetastar[i])
+            
+            modB[i] = sum(self.bmnc[fs,:]*np.cos(
+                self.xmnyq*theta[i] - self.xnnyq*phi[i]))            
+            
+        plt.plot(phi, modB)
+        if show:
+            plt.show()
+        
+    def mirror(self, fs=-1):
+        if fs < 0:
+            fs = self.ns-1
+        B1 = self.modb_at_point(fs, 0, 0)
+        B2 = self.modb_at_point(fs, 0, np.pi/self.nfp)
+        print B1, B2
+        return (B1 - B2)/(B1 + B2)
