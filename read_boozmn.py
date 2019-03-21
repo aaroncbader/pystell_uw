f987aa8c9f3ff257b250f78d0d225f5a065268bb
Aaron Bader <abader@engr.wisc.edu>
2019-03-21 10:27:41 -0500

    finally fix xyz2booz

--- read_boozmn.py
+++ read_boozmn.py
@@ -50,7 +50,7 @@ class boozer:
         #get the z value
         z = self.field_at_point(s, theta, zeta, fourier='z')
         #get the phi value
-        phi = zeta - self.field_at_point(s, theta, zeta, fourier='p')
+        phi = zeta + self.field_at_point(s, theta, zeta, fourier='p')
         return r, z, phi
 
     def booz2xyz(self, s, theta, zeta):
